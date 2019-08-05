31321521e17839d2020ba5607bac09e8677bb425
Miao Yu <miaoyu@umich.edu>
2019-08-05 10:17:17 -0400

    correct ex2

--- machine-learning-ex/ex2/predict.m
+++ machine-learning-ex/ex2/predict.m
@@ -14,8 +14,13 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
+for i = 1:m
+    if sigmoid(X(i,:)*theta)>= 0.5
+        p(i,1) = 1;
+    end
+end
 
-
+% p = sigmoid(X*theta)>=0.5;
 
 
 
